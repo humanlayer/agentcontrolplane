e15d00c689cce84560df3ec57da78edf1f509cf9
Dex <dexter@metalytics.dev>
2025-04-14 16:54:06 -0500

    Test refac (#73)
    * add secret
    
    * move llm
    
    * initial test artifacts refactor
    
    * fix linter

--- acp/internal/controller/task/utils_test.go
+++ acp/internal/controller/task/utils_test.go
@@ -3,199 +3,41 @@ package task
 import (
 	"context"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/humanlayer/agentcontrolplane/acp/test/utils"
+
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 
 	acp "github.com/humanlayer/agentcontrolplane/acp/api/v1alpha1"
-	"go.opentelemetry.io/otel/trace/noop" // Import the noop tracer
+	"go.opentelemetry.io/otel/trace/noop"
 
 	"github.com/humanlayer/agentcontrolplane/acp/internal/mcpmanager"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-// todo this file should probably live in a shared package, but for now...
-type TestSecret struct {
-	name   string
-	secret *corev1.Secret
+var testSecret = &utils.TestSecret{
+	Name: "test-secret",
 }
 
-func (t *TestSecret) Setup(ctx context.Context) *corev1.Secret {
-	By("creating the secret")
-	secret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      t.name,
-			Namespace: "default",
-		},
-		Data: map[string][]byte{
-			"api-key": []byte("test-api-key"),
-		},
-	}
-	err := k8sClient.Create(ctx, secret)
-	Expect(err).NotTo(HaveOccurred())
-	t.secret = secret
-	return secret
+var testLLM = &utils.TestLLM{
+	Name:       "test-llm",
+	SecretName: testSecret.Name,
 }
 
-func (t *TestSecret) Teardown(ctx context.Context) {
-	By("deleting the secret")
-	Expect(k8sClient.Delete(ctx, t.secret)).To(Succeed())
+var testAgent = &utils.TestAgent{
+	Name:         "test-agent",
+	LLMName:      testLLM.Name,
+	SystemPrompt: "you are a testing assistant",
+	MCPServers:   []acp.LocalObjectReference{},
 }
 
-var testSecret = &TestSecret{
-	name: "test-secret",
-}
-
-type TestLLM struct {
-	name string
-	llm  *acp.LLM
-}
-
-func (t *TestLLM) Setup(ctx context.Context) *acp.LLM {
-	By("creating the llm")
-	llm := &acp.LLM{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      t.name,
-			Namespace: "default",
-		},
-		Spec: acp.LLMSpec{
-			Provider: "openai",
-			APIKeyFrom: &acp.APIKeySource{
-				SecretKeyRef: acp.SecretKeyRef{
-					Name: testSecret.name,
-					Key:  "api-key",
-				},
-			},
-		},
-	}
-	err := k8sClient.Create(ctx, llm)
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient.Get(ctx, types.NamespacedName{Name: t.name, Namespace: "default"}, llm)).To(Succeed())
-	t.llm = llm
-	return llm
-}
-
-func (t *TestLLM) SetupWithStatus(ctx context.Context, status acp.LLMStatus) *acp.LLM {
-	llm := t.Setup(ctx)
-	llm.Status = status
-	Expect(k8sClient.Status().Update(ctx, llm)).To(Succeed())
-	t.llm = llm
-	return llm
-}
-
-func (t *TestLLM) Teardown(ctx context.Context) {
-	By("deleting the llm")
-	Expect(k8sClient.Delete(ctx, t.llm)).To(Succeed())
-}
-
-var testLLM = &TestLLM{
-	name: "test-llm",
-}
-
-type TestAgent struct {
-	name       string
-	llmName    string
-	system     string
-	mcpServers []acp.LocalObjectReference
-	agent      *acp.Agent
-}
-
-func (t *TestAgent) Setup(ctx context.Context) *acp.Agent {
-	By("creating the agent")
-	agent := &acp.Agent{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: t.name,
-
-			Namespace: "default",
-		},
-		Spec: acp.AgentSpec{
-			LLMRef: acp.LocalObjectReference{
-				Name: t.llmName,
-			},
-			System:     t.system,
-			MCPServers: t.mcpServers,
-		},
-	}
-	err := k8sClient.Create(ctx, agent)
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient.Get(ctx, types.NamespacedName{Name: t.name, Namespace: "default"}, agent)).To(Succeed())
-	t.agent = agent
-	return agent
-}
-
-func (t *TestAgent) SetupWithStatus(ctx context.Context, status acp.AgentStatus) *acp.Agent {
-	agent := t.Setup(ctx)
-	agent.Status = status
-	Expect(k8sClient.Status().Update(ctx, agent)).To(Succeed())
-	t.agent = agent
-	return agent
-}
-
-func (t *TestAgent) Teardown(ctx context.Context) {
-	By("deleting the agent")
-	Expect(k8sClient.Delete(ctx, t.agent)).To(Succeed())
-}
-
-var testAgent = &TestAgent{
-	name:       "test-agent",
-	llmName:    testLLM.name,
-	system:     "you are a testing assistant",
-	mcpServers: []acp.LocalObjectReference{},
-}
-
-type TestTask struct {
-	name        string
-	agentName   string
-	userMessage string
-	task        *acp.Task
-}
-
-func (t *TestTask) Setup(ctx context.Context) *acp.Task {
-	By("creating the task")
-	task := &acp.Task{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      t.name,
-			Namespace: "default",
-		},
-		Spec: acp.TaskSpec{},
-	}
-	if t.agentName != "" {
-		task.Spec.AgentRef = acp.LocalObjectReference{
-			Name: t.agentName,
-		}
-	}
-	if t.userMessage != "" {
-		task.Spec.UserMessage = t.userMessage
-	}
-
-	err := k8sClient.Create(ctx, task)
-	Expect(err).NotTo(HaveOccurred())
-
-	Expect(k8sClient.Get(ctx, types.NamespacedName{Name: t.name, Namespace: "default"}, task)).To(Succeed())
-	t.task = task
-	return task
-}
-
-func (t *TestTask) SetupWithStatus(ctx context.Context, status acp.TaskStatus) *acp.Task {
-	task := t.Setup(ctx)
-	task.Status = status
-	Expect(k8sClient.Status().Update(ctx, task)).To(Succeed())
-	Expect(k8sClient.Get(ctx, types.NamespacedName{Name: t.name, Namespace: "default"}, task)).To(Succeed())
-	t.task = task
-	return task
-}
-
-func (t *TestTask) Teardown(ctx context.Context) {
-	By("deleting the task")
-	Expect(k8sClient.Delete(ctx, t.task)).To(Succeed())
-}
-
-var testTask = &TestTask{
-	name:        "test-task",
-	agentName:   "test-agent",
-	userMessage: "what is the capital of the moon?",
+var testTask = &utils.TestTask{
+	Name:        "test-task",
+	AgentName:   "test-agent",
+	UserMessage: "what is the capital of the moon?",
 }
 
 type TestToolCall struct {
@@ -210,13 +52,13 @@ func (t *TestToolCall) Setup(ctx context.Context) *acp.ToolCall {
 			Name:      t.name,
 			Namespace: "default",
 			Labels: map[string]string{
-				"acp.humanlayer.dev/task":            testTask.name,
+				"acp.humanlayer.dev/task":            testTask.Name,
 				"acp.humanlayer.dev/toolcallrequest": "test123",
 			},
 		},
 		Spec: acp.ToolCallSpec{
 			TaskRef: acp.LocalObjectReference{
-				Name: testTask.name,
+				Name: testTask.Name,
 			},
 			ToolRef: acp.LocalObjectReference{
 				Name: "test-tool",
@@ -254,12 +96,12 @@ var testToolCallTwo = &TestToolCall{
 
 // nolint:golint,unparam
 func setupSuiteObjects(ctx context.Context) (secret *corev1.Secret, llm *acp.LLM, agent *acp.Agent, teardown func()) {
-	secret = testSecret.Setup(ctx)
-	llm = testLLM.SetupWithStatus(ctx, acp.LLMStatus{
+	secret = testSecret.Setup(ctx, k8sClient)
+	llm = testLLM.SetupWithStatus(ctx, k8sClient, acp.LLMStatus{
 		Status: "Ready",
 		Ready:  true,
 	})
-	agent = testAgent.SetupWithStatus(ctx, acp.AgentStatus{
+	agent = testAgent.SetupWithStatus(ctx, k8sClient, acp.AgentStatus{
 		Status: "Ready",
 		Ready:  true,
 	})
---
+++ acp/test/utils/llm.go
@@ -0,0 +1,64 @@
+package utils
+
+import (
+	"context"
+
+	"github.com/humanlayer/agentcontrolplane/acp/api/v1alpha1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type TestLLM struct {
+	Name       string
+	SecretName string
+	LLM        *v1alpha1.LLM
+	k8sClient  client.Client
+}
+
+func (t *TestLLM) Setup(ctx context.Context, k8sClient client.Client) *v1alpha1.LLM {
+	t.k8sClient = k8sClient
+	By("creating the llm")
+	llm := &v1alpha1.LLM{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      t.Name,
+			Namespace: "default",
+		},
+		Spec: v1alpha1.LLMSpec{
+			Provider: "openai",
+			APIKeyFrom: &v1alpha1.APIKeySource{
+				SecretKeyRef: v1alpha1.SecretKeyRef{
+					Name: t.SecretName,
+					Key:  "api-key",
+				},
+			},
+		},
+	}
+	err := k8sClient.Create(ctx, llm)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient.Get(ctx, types.NamespacedName{Name: t.Name, Namespace: "default"}, llm)).To(Succeed())
+	t.LLM = llm
+	return llm
+}
+
+func (t *TestLLM) SetupWithStatus(
+	ctx context.Context,
+	k8sClient client.Client,
+	status v1alpha1.LLMStatus,
+) *v1alpha1.LLM {
+	llm := t.Setup(ctx, k8sClient)
+	llm.Status = status
+	Expect(k8sClient.Status().Update(ctx, llm)).To(Succeed())
+	t.LLM = llm
+	return llm
+}
+
+func (t *TestLLM) Teardown(ctx context.Context) {
+	if t.k8sClient == nil {
+		return
+	}
+	By("deleting the llm")
+	Expect(t.k8sClient.Delete(ctx, t.LLM)).To(Succeed())
+}
